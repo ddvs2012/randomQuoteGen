b1d12f583106ac400a617da5eb36b84c20d72663
Daniel Alexander <davisd49@msu.edu>
2016-11-30 20:28:28 -0500

    Created quotes array and got some fa icons in, more formatting mainly, random generation functionality working

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-$.ajax({
-	type: 'GET',
-	url: 'sampleAPI'
-	success: function(data) {
-		console.log('success', data);
-	}
-});
- 
-
+ $(document).ready(function() {
+    // Only change code below this line.
+    $("#getMessage").on("click", function(newQuote){
+		newQuote = Math.floor(Math.random() * quotes.length);
+		$(".message").html(quotes[newQuote]);
+    });
+    // Only change code above this line.
+  });
+  
+  quotes = [
+	"Wine is constant proof that God loves us and loves to see us happy. ",
+  
+	"By failing to prepare, you are preparing to fail. ",
+	
+	"Without continual growth and progress, such words as improvement, achievement, and success have no meaning. ",
+	"Early to bed and early to rise makes a man healthy, wealthy and wise.",
+	"Money has never made man happy, nor will it, there is nothing in its nature to produce happiness. The more of it one has the more one wants. ",
+	"Whatever is begun in anger ends in shame."
+	
+  ]
